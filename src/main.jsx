a4773c19a8d9ded65f933242b015f28b718f367f
jesgaminggg <jesgaminggg@gmail.com>
2026-09-08 14:29:52 +0530

    Integrate V2 directly into existing tabs

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,15 @@ import './index.css'
 import './v1.2-performance.js'
 import './v1.2-polish.css'
 import './v1.3-stars.css'
-import './v1.4-record.js'
-import './v1.4-record.css'
-import './v2.css'
-import './v2-record.css'
-import './v2-record.js'
-import './v2-auth-bridge.js'
-import App from './App.jsx'
+import './v2-app.css'
+import AppV2 from './AppV2.jsx'
 import ProfileRequests from './ProfileRequests.jsx'
-import V2FeaturesSafe from './V2FeaturesSafe.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
-      <App />
+      <AppV2 />
       <ProfileRequests />
-      <V2FeaturesSafe />
     </>
   </StrictMode>,
 )
