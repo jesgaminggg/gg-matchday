70afac19c65a15cf3e35aeab330c48884352b0e8
jesgaminggg <jesgaminggg@gmail.com>
2026-09-08 12:02:50 +0530

    Wire v1.4 record UI layer

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import './v1.2-performance.js'
 import './v1.2-polish.css'
 import './v1.3-stars.css'
+import './v1.4-record.js'
+import './v1.4-record.css'
 import App from './App.jsx'
 import ProfileRequests from './ProfileRequests.jsx'
 
