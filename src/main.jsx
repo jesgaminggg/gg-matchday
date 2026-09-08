2d79abf03af3fe6115b9f0364d0d5b1acc8ba088
jesgaminggg <jesgaminggg@gmail.com>
2026-09-08 13:56:24 +0530

    Mount GG Matchday V2 feature hub

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,18 @@ import './v1.2-polish.css'
 import './v1.3-stars.css'
 import './v1.4-record.js'
 import './v1.4-record.css'
+import './v2.css'
+import './v2-record.js'
 import App from './App.jsx'
 import ProfileRequests from './ProfileRequests.jsx'
+import V2Features from './V2Features.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
       <App />
       <ProfileRequests />
+      <V2Features />
     </>
   </StrictMode>,
 )
