20f20b7202cb5d1891cf73c7eb9a719efe990a87
jesgaminggg <jesgaminggg@gmail.com>
2026-09-08 09:19:24 +0530

    Wire v1.2 polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './v1.2-performance.js'
+import './v1.2-polish.css'
 import App from './App.jsx'
 import ProfileRequests from './ProfileRequests.jsx'
 
